5d0c9d01648d3f84e26862fc4fe71a82831bf665
Serious Zainab <85008588+zainab-oss@users.noreply.github.com>
2023-07-22 17:51:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 function preload() {
 	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameover = loadSound("gameover.wav");
+	mario_die = loadSound("mariodie.wav");
+	mario_kick = loadSound("kick.wav");
 	setSprites();
 	MarioAnimation();
 }
